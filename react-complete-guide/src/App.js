004ba8330a75a8b44003b9529c55dd7d4bb53226
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-27 14:38:11 +0100

    using useState fot togle names

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import Person from "./Person/person";
 
-function App() {
+const App = props => {
+  const [statepersons, updatepersons] = useState({
+    persons: [{ name: "Sale" }, { name: "Mirka" }, { name: "Mirko" }]
+  });
+  console.log(statepersons.persons[0].name);
+
+  const swichnameHandler = () => {
+    // console.log("bla");
+    if (statepersons.persons[0].name === "Sale") {
+      updatepersons({
+        persons: [
+          { name: "Aleksandar" },
+          { name: "Mirjana" },
+          { name: "Miroslav" }
+        ]
+      });
+    } else {
+      updatepersons({
+        persons: [{ name: "Sale" }, { name: "Mirka" }, { name: "Mirko" }]
+      });
+    }
+  };
   return (
     <div className="App">
       <h1>good evening ladies and gentlemen !!!</h1>
-      <Person name="Sale" />
-      <Person name="Mirka"> my hobi is rasing</Person>
-      <Person name="Mirko" />
+      <button onClick={swichnameHandler}>swich name</button>
+      <Person name={statepersons.persons[0].name} />
+      <Person name={statepersons.persons[1].name}> my hobi is rasing</Person>
+      <Person name={statepersons.persons[2].name} />
     </div>
   );
-}
-
+};
 export default App;
+/*
+ */
