87d61a702b868edc0f05506c16df4d8eb7d4d803
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-28 17:13:00 +0100

    making new page

---
+++ react-complete-guide/src/newPerson/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import DasPerson from "./desPerson/DasPerson";
+
+class Main extends React.Component {
+  render() {
+    return <DasPerson />;
+  }
+}
+export default Main;
---
+++ react-complete-guide/src/newPerson/desPerson/DasPerson.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DasPerson = () => {
+  return (
+    <div>
+      <p>Ich bin das Person</p>
+    </div>
+  );
+};
+
+export default DasPerson;
