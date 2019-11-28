c6087806295d4d481b8ca92acfd31135785b8e59
SashaPlavi <sasha.bingulac@gmail.com>
2019-11-28 19:14:01 +0100

    togle again

--- react-complete-guide/src/newPerson/Main.js
+++ react-complete-guide/src/newPerson/Main.js
@@ -2,8 +2,39 @@ import React from "react";
 import DasPerson from "./DasPerson/DasPerson";
 
 class Main extends React.Component {
+  state = { show: true };
+
+  toglehandel = () => {
+    if (this.state.show) {
+      this.setState({
+        show: false
+      });
+    } else {
+      this.setState({
+        show: true
+      });
+    }
+  };
   render() {
-    return <DasPerson />;
+    let persons = null;
+
+    if (this.state.show) {
+      persons = (
+        <div>
+          <DasPerson />
+          <DasPerson />
+          <DasPerson />
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <button onClick={this.toglehandel}>show persons</button>
+        <p>good evening</p>
+        {persons}
+      </div>
+    );
   }
 }
 export default Main;
